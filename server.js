e553c06ef8e545e89db71d2cfcff2d7120b1a783
Alejandro <tu_correo@ejemplo.com>
2025-12-11 14:06:40 -0300

    Remove deprecated Mongoose connection options

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.DB || 'mongodb://localhost:27017/biblioteca', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.DB || 'mongodb://localhost:27017/biblioteca')
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
